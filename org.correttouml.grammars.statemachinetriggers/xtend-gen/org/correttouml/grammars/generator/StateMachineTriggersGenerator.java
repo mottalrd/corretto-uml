f27bfa2dd647646b96025f35ab41000c513a90a6
Alfredo Motta <motta@elet.polimi.it>
2013-03-05 12:14:15 +0100

    added the org.correttouml.grammars.* plugins

---
+++ org.correttouml.grammars.statemachinetriggers/xtend-gen/org/correttouml/grammars/generator/StateMachineTriggersGenerator.java
@@ -0,0 +1,11 @@
+package org.correttouml.grammars.generator;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.generator.IFileSystemAccess;
+import org.eclipse.xtext.generator.IGenerator;
+
+@SuppressWarnings("all")
+public class StateMachineTriggersGenerator implements IGenerator {
+  public void doGenerate(final Resource resource, final IFileSystemAccess fsa) {
+  }
+}
