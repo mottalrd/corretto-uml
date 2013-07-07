27dd67a9fe87a05390f4c8ac627e14acb6427d01
Alfredo Motta <motta@elet.polimi.it>
2013-07-07 15:32:53 +0200

    Added some trio operators in zot util

---
+++ org.correttouml.uml2zot/src/org/correttouml/uml2zot/semantics/util/trio/LastTime_ii.java
@@ -0,0 +1,24 @@
+/*
+* Author: Mohammad Mehdi Pourhashem Kallehbasti
+* Mail: pourhashem.kallehbasti@elet.polimi.it
+*/
+
+package org.correttouml.uml2zot.semantics.util.trio;
+
+import org.correttouml.uml2zot.semantics.util.bool.BooleanFormulae;
+
+public class LastTime_ii implements BooleanFormulae {
+
+    BooleanFormulae f;
+    int t;
+
+    public LastTime_ii(BooleanFormulae f, int t) {
+        this.f = f;
+        this.t = t;
+    }
+
+    @Override
+    public String toString() {
+        return "(Lasttime_ii " + f + " " + t + ")";
+    }
+}
