38b5837de10494f3d96d6c08a7c16f691b8d0126
mmpourhashem <mmpourhashem@gmail.com>
2015-09-21 18:19:01 +0200

    Model Statistics

--- org.correttouml.uml2zot/src/org/correttouml/uml2zot/UML2Zot.java
+++ org.correttouml.uml2zot/src/org/correttouml/uml2zot/UML2Zot.java
@@ -1,12 +1,25 @@
 package org.correttouml.uml2zot;
 
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.Console;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Time;
+
+import javax.swing.text.html.HTMLEditorKit.Parser;
 
 import org.apache.log4j.Logger;
 import org.correttouml.uml.MadesModel;
+import org.correttouml.uml.diagrams.sequencediagram.Message;
+import org.correttouml.uml.diagrams.sequencediagram.SequenceDiagram;
+import org.correttouml.uml.diagrams.statediagram.Transition;
+import org.correttouml.uml.diagrams.timeconstraints.TimeConstraint;
 import org.correttouml.uml.helpers.UML2ModelHelper;
 import org.correttouml.uml2zot.semantics.SMadesModel;
 import org.correttouml.uml2zot.semantics.statediagram.SState;
@@ -20,7 +33,7 @@ public class UML2Zot {
 	private static final Logger LOGGER = Logger.getLogger(UML2Zot.class); 
 	
 	private Model uml_model;
-	
+	private String uml_model_file;
 	/** The mades model generated from the UML model */
 	private MadesModel mades_model;
 	
@@ -28,9 +41,57 @@ public class UML2Zot {
 	private SMadesModel s_mades_model;
 	
 	public UML2Zot(String uml_model_file){
+		this.uml_model_file = uml_model_file;
 		this.uml_model=UML2ModelHelper.loadModel(uml_model_file);
 		this.mades_model=new MadesModel(uml_model);
 		this.s_mades_model=new SMadesModel(mades_model);
+	}
+	
+	public String getModelStatistics() throws IOException{
+		int objectsN = 0;
+		int statesN = 0;
+		int transitionsN = 0;
+		int messagesN = 0;
+		int sdTimeConstraintsN = 0;
+		int arithVarsN = 0;
+		for(org.correttouml.uml.diagrams.classdiagram.Class c: this.mades_model.getClassdiagram().getClasses()){
+			for(org.correttouml.uml.diagrams.classdiagram.Object obj: c.getObjects()){
+				objectsN++;
+			}
+			for(org.correttouml.uml.diagrams.statediagram.StateDiagram std: c.getStateDiagrams()){
+				for(Transition t : std.getTransitions()){
+					transitionsN++;
+				}
+				for(org.correttouml.uml.diagrams.statediagram.State s: std.getStates()){
+					for(org.correttouml.uml.diagrams.classdiagram.Object obj: c.getObjects()){
+						statesN++;
+					}
+				}
+			}
+		}
+		for(SequenceDiagram sd: this.mades_model.getSequenceDiagrams()){
+			for (Message m : sd.getMessages()) {
+				messagesN++;
+				}
+			for (TimeConstraint t : sd.getTimeConstraints()) {
+				sdTimeConstraintsN++;
+			}
+		}
+		FileInputStream fstream = new FileInputStream(uml_model_file);
+		BufferedReader in = new BufferedReader(new InputStreamReader(fstream));
+		String readLine = "";
+		int umlElementsN = 0;
+		while((readLine = in.readLine()) != null) {
+		String[] words = readLine.split(" ");
+		for(String s : words) {
+			if(s.startsWith("xmi:id=")) umlElementsN++;}}
+		
+			
+		return ";  Model statistics:\n;  "
+		+ Integer.toString(umlElementsN) + "\t:Number of UML elements\n;  " 
+		+ Integer.toString(objectsN) + "\t:Number of objects\n;  " + Integer.toString(statesN) + "\t:Number of states\n;  "  
+		+ Integer.toString(transitionsN) + "\t:Number of transitions\n;  " + Integer.toString(messagesN) + "\t:Number of messages in sequence diagrams\n;  " + Integer.toString(sdTimeConstraintsN) 
+		+ "\t:Number of time contraints in sequence diagrams\n";
 	}
 	
 	public void generateMappingsFile(File mappings_file){
@@ -48,6 +109,7 @@ public class UML2Zot {
 						SState ss=new org.correttouml.uml2zot.semantics.statediagram.SState(s);
 						Predicate p=ss.getPredicate(obj);
 						out.write(p.getPredicateName()+","+s.getUMLId()+"\n");
+						
 					}
 				}
 			}
@@ -63,7 +125,7 @@ public class UML2Zot {
 		LOGGER.info("Build the ZOT file");
 		ZOTConf zot=new ZOTConf(100, "ae2zot", "z3", this.s_mades_model);
 		try {
-			zot.writeVerificationZOTFile(zot_file);
+			zot.writeVerificationZOTFile(zot_file, getModelStatistics());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -73,7 +135,7 @@ public class UML2Zot {
 		LOGGER.info("Build the ZOT file");
 		ZOTConf zot=new ZOTConf(timebound, plugin, solver, this.s_mades_model);
 		try {
-			zot.writeVerificationZOTFile(zot_file);
+			zot.writeVerificationZOTFile(zot_file, getModelStatistics());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
